a9e1cf3201f17311e73d7e4ab8a24915a4fd1736
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-03-14 18:32:51 -0600

    corrections made to conceptual-data

--- phase3.sql
+++ phase3.sql
@@ -1,31 +1,37 @@
 USE ojonah;
 ALTER DATABASE ojonah CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-DROP TABLE IF EXISTS registration;
-DROP TABLE IF EXISTS course;
-DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS BET;
+DROP TABLE IF EXISTS SPORT;
+DROP TABLE IF EXISTS USER;
 
-CREATE TABLE student(
-	studentId BINARY(16) NOT NULL,
-	studentFirstName VARCHAR (30) NOT NULL,
-	studentLastName VARCHAR (30) NOT NULL,
-	INDEX (studentFirstName),
-	PRIMARY KEY (studentId)
-
+CREATE TABLE `USER` (
+	`userId` BINARY(16) NOT NULL ,
+	`userEmail` VARCHAR(30) NOT NULL ,
+	`userFirstName` VARCHAR(30) NOT NULL ,
+	`userLastName` VARCHAR(30) NOT NULL,
+	PRIMARY KEY (userId),
+	UNIQUE (userEmail,userId),
+	INDEX (userEmail)
 );
 
-CREATE TABLE course(
-	courseId BINARY(16) NOT NULL,
-	courseStudentId BINARY(16) NOT NULL,
-	courseTitle VARCHAR (15) NOT NULL,
-	PRIMARY KEY (courseId),
-	FOREIGN KEY (courseStudentId) REFERENCES student(studentId)
+CREATE TABLE `SPORT` (
+	`sportId` BINARY(32) NOT NULL ,
+	`sportBetType` VARCHAR(15) NOT NULL ,
+	`sportLeague` VARCHAR(25) NOT NULL ,
+	`sportName` VARCHAR(15) NOT NULL,
+	PRIMARY KEY (sportId),
+	UNIQUE (sportBetType),
+	INDEX (sportName,sportLeague)
 );
 
-CREATE TABLE registration(
-   registrationCourseId BINARY(16) NOT NULL ,
-   registrationStudentId BINARY(16) NOT NULL,
-   INDEX (registrationCourseId),
-   INDEX (registrationStudentId),
-   FOREIGN KEY (registrationCourseId) REFERENCES course(courseId),
-   FOREIGN KEY (registrationStudentId) REFERENCES student(studentId)
+CREATE TABLE `BET` (
+	`betId` BINARY(16) NOT NULL ,
+	`betUserId` BINARY(32) NOT NULL ,
+	`betType` VARCHAR(15) NOT NULL ,
+	`betSportId` BINARY(16) NOT NULL,
+	PRIMARY KEY (betId),
+	FOREIGN KEY (betSportId) REFERENCES SPORT(sportId),
+	FOREIGN KEY (betType) REFERENCES SPORT(sportBetType),
+	FOREIGN KEY (betUserId) REFERENCES USER(userId)
 );
+
