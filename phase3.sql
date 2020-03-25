0bf78ff390adfd4ec5e302ee63f4a96959c2ba04
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-03-25 14:38:59 -0600

    corrections made to conceptual-data

--- phase3.sql
+++ phase3.sql
@@ -6,32 +6,30 @@ DROP TABLE IF EXISTS USER;
 
 CREATE TABLE `USER` (
 	`userId` BINARY(16) NOT NULL ,
-	`userEmail` VARCHAR(30) NOT NULL ,
+	`userEmail` VARCHAR(128) NOT NULL ,
 	`userFirstName` VARCHAR(30) NOT NULL ,
 	`userLastName` VARCHAR(30) NOT NULL,
 	PRIMARY KEY (userId),
-	UNIQUE (userEmail,userId),
+	UNIQUE (userEmail),
 	INDEX (userEmail)
 );
 
 CREATE TABLE `SPORT` (
-	`sportId` BINARY(32) NOT NULL ,
-	`sportBetType` VARCHAR(15) NOT NULL ,
-	`sportLeague` VARCHAR(25) NOT NULL ,
-	`sportName` VARCHAR(15) NOT NULL,
+	`sportId` BINARY(16) NOT NULL ,
+	`sportLeague` VARCHAR(30) NOT NULL ,
+	`sportName` VARCHAR(30) NOT NULL,
 	PRIMARY KEY (sportId),
-	UNIQUE (sportBetType),
 	INDEX (sportName,sportLeague)
 );
 
 CREATE TABLE `BET` (
 	`betId` BINARY(16) NOT NULL ,
-	`betUserId` BINARY(32) NOT NULL ,
+	`betUserId` BINARY(16) NOT NULL ,
 	`betType` VARCHAR(15) NOT NULL ,
 	`betSportId` BINARY(16) NOT NULL,
 	PRIMARY KEY (betId),
 	FOREIGN KEY (betSportId) REFERENCES SPORT(sportId),
-	FOREIGN KEY (betType) REFERENCES SPORT(sportBetType),
-	FOREIGN KEY (betUserId) REFERENCES USER(userId)
+	FOREIGN KEY (betUserId) REFERENCES USER(userId),
+INDEX (betSportId,betUserId)
 );
 
